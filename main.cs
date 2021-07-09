4b10bb78955044b09ba411031ed611ddea08acd4
Marcosffn <67158009+Marcosffn@users.noreply.github.com>
2021-07-09 14:31:03 -0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -13,59 +13,59 @@ void Main()
 
 public class Robo
 {
-    float SensorLuz1;
-    float SensorLuz2;
-    float SensorLuz3;
-    float SensorLuz4;
-    float SensorLuz5;
-    float SensorLuz6;
+    float sensorLuz1;
+    float sensorLuz2;
+    float sensorLuz3;
+    float sensorLuz4;
+    float sensorLuz5;
+    float sensorLuz6;
 
-    String SensorCor1;
-    String SensorCor2;
-    String SensorCor3;
-    String SensorCor4;
-    String SensorCor5;
-    String SensorCor6;
+    String sensorCor1;
+    String sensorCor2;
+    String sensorCor3;
+    String sensorCor4;
+    String sensorCor5;
+    String sensorCor6;
 
     public void RealizarLeituras()
     {
-        this.SensorLuz1 = bc.Lightness(0);
-        this.SensorLuz2 = bc.Lightness(1);
-        this.SensorLuz3 = bc.Lightness(2);
-        this.SensorLuz4 = bc.Lightness(3);
-        this.SensorLuz5 = bc.Lightness(4);
-        this.SensorLuz6 = bc.Lightness(5);
+        this.sensorLuz1 = bc.Lightness(0);
+        this.sensorLuz2 = bc.Lightness(1);
+        this.sensorLuz3 = bc.Lightness(2);
+        this.sensorLuz4 = bc.Lightness(3);
+        this.sensorLuz5 = bc.Lightness(4);
+        this.sensorLuz6 = bc.Lightness(5);
 
-        this.SensorCor1 = bc.ReturnColor(0);
-        this.SensorCor2 = bc.ReturnColor(1);
-        this.SensorCor3 = bc.ReturnColor(2);
-        this.SensorCor4 = bc.ReturnColor(3);
-        this.SensorCor5 = bc.ReturnColor(4);
-        this.SensorCor6 = bc.ReturnColor(5);
+        this.sensorCor1 = bc.ReturnColor(0);
+        this.sensorCor2 = bc.ReturnColor(1);
+        this.sensorCor3 = bc.ReturnColor(2);
+        this.sensorCor4 = bc.ReturnColor(3);
+        this.sensorCor5 = bc.ReturnColor(4);
+        this.sensorCor6 = bc.ReturnColor(5);
     }
 
     public void PrintarLeituras(string tipo=null)
     {
         if (tipo == "Cor")
         {
-            bc.PrintConsole(0, $"Sensor 1: {this.SensorCor1}, Sensor 2: {this.SensorCor2}");
-            bc.PrintConsole(1, $"Sensor 3: {this.SensorCor3}, Sensor 4: {this.SensorCor4}");
-            bc.PrintConsole(2, $"Sensor 5: {this.SensorCor5}, Sensor 6: {this.SensorCor6}");
+            bc.PrintConsole(0, $"sensor 1: {this.sensorCor1}, sensor 2: {this.sensorCor2}");
+            bc.PrintConsole(1, $"sensor 3: {this.sensorCor3}, sensor 4: {this.sensorCor4}");
+            bc.PrintConsole(2, $"sensor 5: {this.sensorCor5}, sensor 6: {this.sensorCor6}");
         }
         else if (tipo == "Luz")
         {
-            bc.PrintConsole(0, $"Sensor 1: {this.SensorLuz1.ToString("00.00")}, Sensor 2: {this.SensorLuz2.ToString("00.00")}");
-            bc.PrintConsole(1, $"Sensor 3: {this.SensorLuz3.ToString("00.00")}, Sensor 4: {this.SensorLuz4.ToString("00.00")}");
-            bc.PrintConsole(2, $"Sensor 5: {this.SensorLuz5.ToString("00.00")}, Sensor 6: {this.SensorLuz6.ToString("00.00")}");
+            bc.PrintConsole(0, $"sensor 1: {this.sensorLuz1.ToString("00.00")}, sensor 2: {this.sensorLuz2.ToString("00.00")}");
+            bc.PrintConsole(1, $"sensor 3: {this.sensorLuz3.ToString("00.00")}, sensor 4: {this.sensorLuz4.ToString("00.00")}");
+            bc.PrintConsole(2, $"sensor 5: {this.sensorLuz5.ToString("00.00")}, sensor 6: {this.sensorLuz6.ToString("00.00")}");
         }
         else if (tipo == null)
         {
-            bc.PrintConsole(0, $"Sensor 1: {this.SensorLuz1.ToString("00.00")}, Sensor 2: {this.SensorLuz2.ToString("00.00")}"
-            + " -- " + $"Sensor 1: {this.SensorCor1}, Sensor 2: {this.SensorCor2}");
-            bc.PrintConsole(1, $"Sensor 3: {this.SensorLuz3.ToString("00.00")}, Sensor 4: {this.SensorLuz4.ToString("00.00")}"
-            + " -- " + $"Sensor 3: {this.SensorCor3}, Sensor 4: {this.SensorCor4}");
-            bc.PrintConsole(2, $"Sensor 5: {this.SensorLuz5.ToString("00.00")}, Sensor 6: {this.SensorLuz6.ToString("00.00")}"
-            + " -- " + $"Sensor 5: {this.SensorCor5}, Sensor 6: {this.SensorCor6}");
+            bc.PrintConsole(0, $"sensor 1: {this.sensorLuz1.ToString("00.00")}, sensor 2: {this.sensorLuz2.ToString("00.00")}"
+            + " -- " + $"sensor 1: {this.sensorCor1}, sensor 2: {this.sensorCor2}");
+            bc.PrintConsole(1, $"sensor 3: {this.sensorLuz3.ToString("00.00")}, sensor 4: {this.sensorLuz4.ToString("00.00")}"
+            + " -- " + $"sensor 3: {this.sensorCor3}, sensor 4: {this.sensorCor4}");
+            bc.PrintConsole(2, $"sensor 5: {this.sensorLuz5.ToString("00.00")}, sensor 6: {this.sensorLuz6.ToString("00.00")}"
+            + " -- " + $"sensor 5: {this.sensorCor5}, sensor 6: {this.sensorCor6}");
         }
 
     }
